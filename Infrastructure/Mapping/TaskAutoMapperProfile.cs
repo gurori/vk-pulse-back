0b540ce6e11396ede18bf90fe199d4e5c437cf5d
Vasilev Valery <vvi.20030613@gmail.com>
2025-12-05 10:49:28 +0900

    test

---
+++ Infrastructure/Mapping/TaskAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Models.Tasks;
+
+namespace Infrastructure.Mapping
+{
+    public class TaskAutoMapperProfile : Profile
+    {
+        public TaskAutoMapperProfile()
+        {
+            CreateMap<TaskEntity, TaskResponse>();
+        }
+    }
+}
---
+++ Infrastructure/Mapping/TeamAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Models.Teams;
+
+namespace Infrastructure.Mapping
+{
+    public class TeamAutoMapperProfile : Profile
+    {
+        public TeamAutoMapperProfile()
+        {
+            CreateMap<TeamEntity, TeamResponse>();
+        }
+    }
+}
